24afb96fee1372069f60b0c9bd294bc5adee2498
Thalaios <JOAONETOO1905@GMAIL.COM>
2025-05-30 03:42:02 +0000

    Implementação dos movimentos do cavalo

--- xadrez_novato.c
+++ xadrez_novato.c
@@ -3,20 +3,21 @@
 int main() {
 
     //variaveis para escolhas do usuario
-    char direcao[40];
+    char direcao[40], direcao1[30];
     int escolhaJogador, numeroMovimentos;
-    int rainha = 1, bispo = 1, torre = 1;
+    int rainha = 1, bispo = 1, torre = 1, cavalo = 1;
  
     //Menu de escolha para qual peça será movida
     printf("Escolha uma peça para mover:\n");
     printf("1 - Rainha\n");
     printf("2 - Torre\n");
     printf("3 - Bispo\n");   
-    printf("4 - Sair\n");
+    printf("4 - Cavalo\n");  
+    printf("5 - Sair\n");
     scanf("%d", &escolhaJogador);
 
     //Verificação se a escolha é valida
-    while (escolhaJogador < 1 || escolhaJogador > 4){
+    while (escolhaJogador < 1 || escolhaJogador > 5){
         printf("Escolha inválida! Tente novamente:\n");
         scanf("%d", &escolhaJogador);
     }
@@ -58,9 +59,22 @@ int main() {
         for (bispo = 1; bispo <= numeroMovimentos; bispo++) 
         {
           printf("Bispo se moveu para %s uma vez\n", direcao);
-        }
+        } /// ver como melhorar os movimentos do bispo
         break;
     case 4:
+
+    printf("Escolha se o cavalo ira para frente, para tras ou para os lados:\n");
+    scanf("%s", direcao);
+    printf("Escolha se o cavalo ira a esquerda ou direita, ao percorrer as duas casas,:\n");
+    scanf("%s", direcao1);
+
+        for (cavalo = 1; cavalo <= 2; cavalo++) 
+        { 
+          printf("Cavalo se moveu para cima uma vez\n");
+        }
+          printf("Cavalo se moveu para Direita uma vez\n");
+        break;
+    case 5:
     {
         printf("Saindo...\n");
     }
